2a578e2e007eb414b32d53687cfcd08662189fa6
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-07-30 17:31:27 +0200

    React Icons Package and FaMapMarker in JobListing Component

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { FaMapMarker } from 'react-icons/fa'; // FaMapMarker is an icon of the list of react (fontawesome) icons
 
 const JobListing = ({ job }) => {
   const [showFullDescription, setShowFullDescription] = useState(false);  // To either show the full discription or just the first part of it - default is false -> don't show all
@@ -31,6 +32,7 @@ const JobListing = ({ job }) => {
 
         <div className='flex flex-col lg:flex-row justify-between mb-4'>
           <div className='text-orange-700 mb-3'>
+            <FaMapMarker className='inline text-lg mb-1 mr-1' />
             {job.location}
           </div>
           <a
