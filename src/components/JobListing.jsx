3df7a30ea2ac310d208fd5943e4e531a7c9cf9d9
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-07-30 18:59:44 +0200

    Link (from 'react-router-dom') to all Components

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { FaMapMarker } from 'react-icons/fa'; // FaMapMarker is an icon of the list of react (fontawesome) icons
+import { Link } from 'react-router-dom';
 
 const JobListing = ({ job }) => {
   const [showFullDescription, setShowFullDescription] = useState(false);  // To either show the full discription or just the first part of it - default is false -> don't show all
@@ -35,12 +36,12 @@ const JobListing = ({ job }) => {
             <FaMapMarker className='inline text-lg mb-1 mr-1' />
             {job.location}
           </div>
-          <a
+          <Link
             to={`/jobs/${job.id}`}
             className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'
           >
             Read More
-          </a>
+          </Link>
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from '../assets/images/logo.png';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const linkClass = ({ isActive }) =>
@@ -11,23 +12,23 @@ const Navbar = () => {
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
         <div className='flex h-20 items-center justify-between'>
           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-            <a className='flex flex-shrink-0 items-center mr-4' to='/'>
+            <NavLink className='flex flex-shrink-0 items-center mr-4' to='/'>
               <img className='h-10 w-auto' src={logo} alt='React Jobs' />
               <span className='hidden md:block text-white text-2xl font-bold ml-2'>
                 React Jobs
               </span>
-            </a>
+            </NavLink>
             <div className='md:ml-auto'>
               <div className='flex space-x-2'>
-                <a to='/' className={linkClass}>
+                <NavLink to='/' className={linkClass}>
                   Home
-                </a>
-                <a to='/jobs' className={linkClass}>
+                </NavLink>
+                <NavLink to='/jobs' className={linkClass}>
                   Jobs
-                </a>
-                <a to='/add-job' className={linkClass}>
+                </NavLink>
+                <NavLink to='/add-job' className={linkClass}>
                   Add Job
-                </a>
+                </NavLink>
               </div>
             </div>
           </div>
