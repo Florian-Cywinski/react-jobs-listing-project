ea4d7f1171d30aefe6d55c99cf385100927dc4e7
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-07-30 18:00:14 +0200

    React Router Setup - Create Routes From Elements - Router Provider - Homepage Component/Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  )
-}
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route index element={<HomePage />} />
 
-export default App
+    )
+  );
+
+  return <RouterProvider router={router} />;
+};
+export default App;
