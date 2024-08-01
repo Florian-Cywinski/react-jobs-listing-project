e73cad5b5254784921452fa9a4105806684f17ae
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-08-01 11:59:05 +0200

    useEffect() & Data Fetching

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,28 @@
 import JobListing from './JobListing';
-import jobs from '../jobs.json'   // To import the jobs array (later an object)
+// import jobs from '../jobs.json'   // To import the jobs array (later an object)
+import { useState, useEffect } from 'react';  // useEffect allows the component to have side effects (here fetching data when the component renders) - useState to put in the fetched data
 
 const JobListings = ({ isHome = false }) => {   // isHome comes from HomePage.jsx (<JobListings isHome={true} />)
-  const jobListings = isHome ?  jobs.slice(0, 3) : jobs;  // To only show 3 jobs on the home page - on the jobs page it shows all
+  // const jobListings = isHome ?  jobs.slice(0, 3) : jobs;  // To only show 3 jobs on the home page - on the jobs page it shows all
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally { // This code will run independent whether the try or the catch part runs before
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []); // [] is the dependency array
 
   return (
     <section className='bg-blue-50 px-4 py-10'>
@@ -11,7 +31,7 @@ const JobListings = ({ isHome = false }) => {   // isHome comes from HomePage.js
           {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            {jobListings.map((job) => (
+            {jobs.map((job) => (
               <JobListing key={job.id} job={job} />
             ))}
           </div>
